4197b0d783318b8e5e9f28f356f7ad03c3f89427
Samiratu <samiratuntohsi@gmail.com>
2022-08-23 03:06:43 -0700

    fixed issue

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const myVar = process.argv;
 if (Number(myVar[2]) !== 'undefined' && !isNaN(parseInt(myVar[2]))) {
-  for (let i = 0; i < Number(myVar[2]); i++) {
-    console.log('C is fun');
-  };	  
+  for (let i = 0; i < Number(myVar[2]); i++) { console.log('C is fun'); }
 } else {
   console.log('Missing number of occurrences');
 }

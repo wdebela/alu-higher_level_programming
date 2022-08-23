27043e59b9850eeff5502944550135ae7b1dfda2
Samiratu <samiratuntohsi@gmail.com>
2022-08-23 05:30:00 -0700

    factorial added

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+function factorial(myVar) {
+  let answer = 1;
+  if (typeof myVar[2] === 'undefined' || Number(myVar[2]) === 0 || Number(myVar[2]) === 1) {
+    return answer;
+  } else {
+    for (let i = Number(myVar[2]); i >= 1; i--){ answer = answer * i; }
+ return answer;
+} 
+}
+
+const myVar1 = process.argv;
+answer = factorial(myVar1);
+console.log(answer);

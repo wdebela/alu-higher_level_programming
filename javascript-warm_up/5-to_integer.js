bb00c90e1078e758d5ce9b26e6c1c92b083c67e4
Samiratu <samiratuntohsi@gmail.com>
2022-08-23 01:36:32 -0700

    removed space

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@ const myVar = process.argv;
 if (myVar[2] === 'undefined') {
   console.log('Not a number');
 } else if (Number(myVar[2]) !== 'undefined' && !isNaN(parseInt(myVar[2]))) {
-  console.log('My number : ' + Math.floor(Number(myVar[2])));
+  console.log('My number: ' + Math.floor(Number(myVar[2])));
 } else {
   console.log('Not a number');
 }

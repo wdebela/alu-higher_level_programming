b1e27717b2a0accac5905c20312795bf2d959772
Samiratu <samiratuntohsi@gmail.com>
2022-08-22 21:47:35 -0700

    testing

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const myArgs = process.argv.slice(2);
-if (myArgs.length >= 1) {
-  console.log(myArgs[0]);
-} else {
+if (myArgs === undefined) {
   console.log('No argument');
+} else {
+  console.log(myArgs[0]);
 }

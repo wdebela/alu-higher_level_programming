43742eea40ada9aa2f6bf0bc0165a57992a09a78
Samiratu <samiratuntohsi@gmail.com>
2022-08-22 23:57:27 -0700

    added concat

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const myVar = process.argv;
+if (typeof myVar[2] === 'undefined') {
+  console.log('undefined is undefined');
+} else if (myVar[3] === 'undefined') {
+  let myVar2 = ' is undefined';
+  console.log(myVar[2].concat(myVar2));
+} else {
+  console.log(myVar[2]+ ' is ' + myVar[3]);
+}

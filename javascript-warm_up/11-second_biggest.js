17a5eb6f7839232e9942033783cc27bb015c237a
Samiratu <samiratuntohsi@gmail.com>
2022-08-23 07:29:11 -0700

    exercise 11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -6,6 +6,6 @@ if (typeof myVar[2] === 'undefined') {
   console.log(0);
 } else {
   let Arr = myVar.slice(2).map(Number);
-  Arr = Arr.sort(function(a, b){return b-a});
+  Arr = Arr.sort(function (a, b) { return b - a; });
   console.log(Arr[1]);
 }

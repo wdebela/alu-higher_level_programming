9d9a4816448b2a1515e58ceafad80a3eda4114d9
Samiratu <samiratuntohsi@gmail.com>
2022-08-25 04:21:47 -0700

    added task1

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h){
+  constructor (w, h) {
     this.width = w;
-    this.height= h;
+    this.height = h;
   }
-
 }
 module.exports.Rectangle = Rectangle;
